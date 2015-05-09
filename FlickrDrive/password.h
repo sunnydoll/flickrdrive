158d7f3f9a39e1190c9d0ce03563f3f72d2343ee
Zhichao <silverwindcao@gmail.com>
2015-05-08 23:03:13 -0400

    Pushing files to git

---
+++ FlickrDrive/password.h
@@ -0,0 +1,6 @@
+// Due to security, the Flickr API Secret key is not included in this distribution.
+// Create a password.h file in the \FlickrDrive folder with the following lines:
+  #define FLICKR_API_KEY             _T("33333333333333333333333333333333")
+  #define FLICKR_SECRET_KEY          _T("0123456789012345")
+// where 333... is the Flickr key and 0123... is the secret. Both can be created 
+// from the Flickr website.
